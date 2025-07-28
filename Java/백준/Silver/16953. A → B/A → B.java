15a6322d44e94903bd94d306924b6e2f5fda0bb7
오지환 <116332741+returntoOZ@users.noreply.github.com>
2025-07-28 15:06:04 +0900

    [Silver II] Title: A → B, Time: 200 ms, Memory: 25708 KB -BaekjoonHub

---
+++ Java/백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class node {
+        long next;
+        int cnt;
+
+        node(long next, int cnt){
+            this.next = next;
+            this.cnt = cnt;
+        }
+    }
+
+    static int A,B;
+
+    static void bfs(int start){
+        HashMap<Long, Integer> map = new HashMap<>(); // 중복 체크용 map
+        Queue<node> q = new ArrayDeque<>();
+        q.offer(new node(start, 1));
+
+        while(!q.isEmpty()){
+            node cur = q.poll();
+            if(map.containsKey(cur.next)) continue;
+            map.put(cur.next, map.getOrDefault(cur.next, 0) + 1);
+
+            long first = cur.next * 2;
+            if(first < B){
+                q.offer(new node(first, cur.cnt+1));
+            }else if(first == B){
+                System.out.print(cur.cnt+1);
+                return;
+            }
+
+            long second = cur.next * 10 + 1;
+            if(second < B){
+                q.offer(new node(second, cur.cnt+1));
+            }else if(second == B){
+                System.out.print(cur.cnt+1);
+                return;
+            }
+        }
+        System.out.print(-1);
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        bfs(A);
+    }
+}
