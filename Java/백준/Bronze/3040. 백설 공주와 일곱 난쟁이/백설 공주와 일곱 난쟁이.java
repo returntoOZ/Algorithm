f9e63c786a2bc154c50693663e2a004155b05868
오지환 <116332741+returntoOZ@users.noreply.github.com>
2025-08-09 16:56:21 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- Java/백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ Java/백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -27,6 +27,26 @@ public class Main {
         }
     }
 
+    static void powerset(int cnt, int idx, int sum){
+        if(cnt == 7){
+            if(sum == 100){
+                for(int i=0; i<9; i++){
+                    if(check[i]) sb.append(arr[i]).append("\n");
+                }
+                System.out.print(sb);
+                return;
+            }
+
+        }
+
+        if(idx >= 9) return;
+
+        check[idx] = true;
+        powerset(cnt+1, idx+1, sum + arr[idx]);
+        check[idx] = false;
+        powerset(cnt, idx+1, sum);
+    }
+
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -42,9 +62,10 @@ public class Main {
 
         // 1번 풀이
         // 9개 중에 2개 뽑기
-        combination(0,0, 0);
-        System.out.print(sb);
+//        combination(0,0, 0);
+//        System.out.print(sb);
         // 2번 풀이
-
+        // 7개짜리 부분 집합 만들기
+        powerset(0,0, 0);
     }
 }
