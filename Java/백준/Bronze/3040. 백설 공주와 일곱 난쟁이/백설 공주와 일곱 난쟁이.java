43961f2c785d1e59af37eca92f5a351b325c9390
오지환 <116332741+returntoOZ@users.noreply.github.com>
2025-08-09 16:42:19 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ Java/백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    static boolean[] check;
+    static int sum = 0;
+
+    static StringBuilder sb = new StringBuilder();
+
+    static void combination(int cur, int cmp, int prev){
+        if(cur == 2){
+            if(sum-cmp == 100){
+                for(int i=0; i<9; i++){
+                    if(check[i]) continue;
+                    sb.append(arr[i]).append("\n");
+                }
+                return;
+            }
+        }
+
+        for(int i=prev; i<9; i++){
+            if(check[i]) continue;
+            check[i] = true;
+            combination(cur+1, cmp+arr[i], i+1);
+            check[i] = false;
+        }
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        arr = new int[9];
+        check = new boolean[9];
+        Arrays.fill(check, false);
+
+        for(int i=0; i<9; i++){
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
+            sum += num;
+        }
+
+        // 1번 풀이
+        // 9개 중에 2개 뽑기
+        combination(0,0, 0);
+        System.out.print(sb);
+        // 2번 풀이
+
+    }
+}
