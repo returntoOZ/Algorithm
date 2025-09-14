d705c8400a4d89401a91d993e505837c83a370ad
오지환 <116332741+returntoOZ@users.noreply.github.com>
2025-09-14 11:52:53 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 88.4 MB -BaekjoonHub

--- Java/프로그래머스/2/42578. 의상/의상.java
+++ Java/프로그래머스/2/42578. 의상/의상.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> hash = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         
         for(String[] cloth : clothes){
             String type = cloth[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+        
+        int ans = 1;
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            String key = entry.getKey();
+            Integer value = entry.getValue();
             
-            hash.put(type, hash.getOrDefault(type, 0) + 1);
+            ans *= (value + 1);
         }
-        
-        int answer = 1;
-        
-        for(int v : hash.values()){
-            answer *= (v+1);
-        }
-        return answer-1;
+        return ans - 1;
     }
 }
